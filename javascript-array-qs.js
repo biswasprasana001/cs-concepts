9a187c2b406823a19a6d9e3c6be80cc0290cde51
Biswas Prasana Swain <90154858+biswasprasana001@users.noreply.github.com>
2024-10-06 11:42:49 +0530

    Update javascript-array-qs.js
    Practiced some Array Questions

--- javascript-array-qs.js
+++ javascript-array-qs.js
@@ -1,84 +1,409 @@
-// reverse an array without using the built-in reverse() method
+function sumArray(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
+}
 
-function reverseArrayOptimized(arr) {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left < right) {
-      // Swap elements using destructuring assignment
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-      // Move towards the middle
-      left++;
-      right--;
+function removeDuplicates(arr) {
+  let uniqueArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArr.includes(arr[i])) {
+      uniqueArr.push(arr[i]);
+    }
+  }
+  return uniqueArr;
+}
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+function removeDuplicates(arr) {
+  let uniqueSet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueSet.has(arr[i])) {
+      uniqueSet.add(arr[i]);
+    }
+  }
+  return Array.from(uniqueSet);
+}
+
+function flattenArray(arr) {
+  let result = [];
+  arr.forEach(element => {
+    if (Array.isArray(element)) {
+      result = result.concat(flattenArray(element));
+    } else {
+      result.push(element);
+    }
+  })
+  return result;
+}
+
+function flattenArray(arr) {
+    return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenArray(val)) : acc.concat(val), []);
+}
+
+
+function mergeArrays(arr1, arr2) {
+  let mergedArray = [];
+  let i = 0, j = 0;
+  
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
+  
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i])
+    i++;
+  }
+  
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j])
+    j++;
+  }
+  
+  return mergedArray;
+}
+
+function findCommonElements(arr1, arr2) {
+  let commonElements = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] == arr2[j] && !commonElements.includes(arr1[i])) {
+        commonElements.push(arr1[i]);
+      }
+    }
+  }
+  return commonElements;
+}
+
+function findCommonElements(arr1, arr2) {
+  let set1 = new Set(arr1);
+  let commonElements = arr2.filter(element => set1.has(element));
+  return commonElements;
+}
+
+function findCommonElements(arr1, arr2) {
+  let map = new Map();
+  arr1.forEach(element => map.set(element, true));
+  let commonElements = arr2.filter(element => map.has(element));
+  return commonElements;
+}
+
+function rotateArray(arr, k) {
+  for (let i = 0; i < k; i++) {
+    arr.unshift(arr.pop());
   }
   return arr;
 }
 
-// Example usage:
-let originalArray3 = [1, 2, 3, 4, 5];
-let reversedArray3 = reverseArrayOptimized(originalArray3);
-console.log(reversedArray3); // Output: [5, 4, 3, 2, 1]
+function rotateArray(arr, k) {
+  function reverse(arr, start, end) {
+    while(start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
+  }
+  
+  k = k % arr.length;
+  reverse(arr, 0, arr.length - 1);
+  reverse(arr, 0, k - 1);
+  reverse(arr, k, arr.length - 1);
+  return arr;
+}
 
+function findMissingNumber(arr) {
+  let n = arr.length + 1;
+  for (let i = 1; i <= n; i++) {
+    if (!arr.includes(i)) {
+      return i;
+    }
+  }
+}
 
-// remove duplicate values from an array
+function findMissingNumber(arr) {
+  let n = arr.length + 1;
+  let actualSum = arr.reduce((acc, val) => acc + val, 0);
+  let expectedSum = (n * (n + 1)) / 2;
+  return expectedSum - actualSum;
+}
 
-function removeDuplicatesUsingObject(arr) {
-  let uniqueArray = [];
-  let seen = {};
+function isPalindrome (arr) {
+  let reversedArr = arr.slice().reverse();
   for (let i = 0; i < arr.length; i++) {
-      if (!seen[arr[i]]) {
-          uniqueArray.push(arr[i]);
-          seen[arr[i]] = true;
-      }
+    if (arr[i] !== reversedArr[i]) {
+      return false;
+    }
   }
-  return uniqueArray;
+  return true;
 }
 
-// Example usage:
-let originalArray2 = [1, 2, 2, 3, 4, 4, 5];
-let uniqueArray2 = removeDuplicatesUsingObject(originalArray2);
-console.log(uniqueArray2); // Output: [1, 2, 3, 4, 5]
-
-// maximum and minimum values in an array
-
-function findMaxMinSinglePass(arr) {
-  let max = arr[0];
-  let min = arr[0];
-  
-  for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > max) {
-          max = arr[i];
-      }
-      if (arr[i] < min) {
-          min = arr[i];
-      }
+function isPalindrome(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    if (arr[start] !== arr[end]) {
+      return false;
+    }
+    start++;
+    end--;
   }
+  return true;
+}
+
+function isPalindrome(arr) {
+  return arr.every((val, index) => val == arr[arr.length - 1 - index]);
+}
+
+function chunkArray (arr, size) {
+  let result = [];
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+  return result;
+}
+
+function chunkArray(arr, size) {
+  return arr.reduce((acc, val, index) => {
+    if (index % size == 0) acc.push([]);
+    acc[acc.length - 1].push(arr[index]);
+    return acc;
+  }, [])
+}
+
+function findPairs(arr, target) {
+  let pairs = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] == target) {
+        pairs.push([arr[i], arr[j]]);
+      }
+    }
+  }
+  return pairs;
+}
+
+function findPairs(arr, target) {
+  let pairs = [];
+  let seen = new Set();
+  for (let num of arr) {
+    let complement = target - num;
+    if (seen.has(complement)) {
+      pairs.push([complement, num]);
+    }
+    seen.add(num);
+  }
+  return pairs;
+}
+
+function findPairs(arr, target) {
+  arr.sort((a, b) => a - b);
+  let pairs = [];
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    if (arr[start] + arr[end] == target) {
+      pairs.push([arr[start], arr[end]]);
+      start++;
+      end--;
+    }
+    else if (arr[start] + arr[end] < target) {
+      start++;
+    } else {
+      end--;
+    }
+  }
+  return pairs;
+}
+
+function firstNonRepeatingElement(arr) {
+  for (let i = 0; i < arr.length; i++) {
+  let isRepeating = false;
+    for (let j = 0; j < arr.length; j++) {
+      if (i != j && arr[i] == arr[j]) {
+        isRepeating = true;
+        break;
+      }
+    }
+    if (!isRepeating) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+function firstNonRepeatingElement(arr) {
+  let frequencyMap = new Map();
+  for (let num of arr) {
+    frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
+  }
+  for(let num of arr){
+    if (frequencyMap.get(num) === 1) {
+      return num;
+    }
+  }
+  return null;
+}
+
+function firstNonRepeatingElement(arr) {
+  let frequencyMap = arr.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1;
+    return acc;
+  }, {})
   
-  return { max, min };
+  for (let num of arr) {
+    if (frequencyMap[num] == 1) {
+      return num;
+    }
+  }
+  return null;
 }
 
-// Example usage:
-let originalArray2 = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-let result2 = findMaxMinSinglePass(originalArray2);
-console.log(result2); // Output: { max: 9, min: 1 }
-
-// sum of all elements in an array
-
-const sumArrayOptimized = arr => arr.reduce((acc, val) => acc + val, 0);
-
-// Example usage:
-let originalArray3 = [1, 2, 3, 4, 5];
-let sum3 = sumArrayOptimized(originalArray3);
-console.log(sum3); // Output: 15
-
-// rotate an array to the right by a given number of positions
-
-function rotateArrayOptimized(arr, positions) {
-  let length = arr.length;
-  positions = positions % length; // Handle cases where positions > length
-  return arr.slice(-positions).concat(arr.slice(0, -positions));
+function moveZerosToEnd(arr) {
+  let result = [];
+  let zeros = [];
+  for (let num of arr) {
+    if (num == 0) {
+      zeros.push(num);
+    } else {
+      result.push(num);
+    }
+  }
+  return result.concat(zeros);
 }
 
-// Example usage:
-let originalArray3 = [1, 2, 3, 4, 5];
-let rotatedArray3 = rotateArrayOptimized(originalArray3, 2);
-console.log(rotatedArray3); // Output: [4, 5, 1, 2, 3]
+function moveZerosToEnd(arr) {
+  let index = 0;
+  for (let num of arr) {
+    if (num !== 0) {
+      arr[index++] = num;
+    }
+  }
+  for (let i = index; i < arr.length; i++) {
+    arr[i] = 0;
+  }
+  return arr;
+}
+
+function moveZerosToEnd(arr) {
+  let nonZeros = arr.filter(num => num !== 0);
+  let zeros = arr.filter(num => num == 0);
+  return nonZeros.concat(zeros);
+}
+
+
+function longestConsecutiveSequence(arr) {
+  let numSet = new Set(arr);
+  return arr.reduce((longestStreak, num) => {
+    if (!numSet.has(num - 1)) {
+      let currentStreak = 1;
+      let currentNum = num;
+      while(numSet.has(currentNum + 1)) {
+        currentStreak++;
+        currentNum++;
+      }
+      return Math.max(longestStreak, currentStreak);
+    }
+    return longestStreak;
+  }, 0)
+}
+
+function maxProductBruteForce(arr) {
+  let maxProduct = -Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+            let product = arr[i] * arr[j];
+            if (product > maxProduct) {
+                maxProduct = product;
+            }
+    }
+  }
+  return maxProduct;
+}
+
+function maxProductUsingSorting(arr) {
+  arr.sort((a, b) => a - b);
+  n = arr.length;
+  return Math.max(arr[0] * arr[1], arr[n - 1] * arr[n - 2]);
+}
+
+function maxProductUsingLinearScan(arr) {
+  let max1 = -Infinity, max2 = -Infinity;
+  let min1 = Infinity. min2 = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max1) {
+      max2 = max1;
+      max1 = arr[i];
+    } else if (arr[i] > max2) {
+      max2 = arr[i];
+    }
+    
+    if (arr[i] < min1) {
+      min2 = min1;
+      min1 = arr[i];
+    } else if (arr[i] < min2) {
+      min2 = arr[i];
+    }
+  }
+  return Math.max(max1 * max2, min1 * min2);
+}
+
+function findMajorityElementBruteForce(arr) {
+  let n = arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    let count = 1;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        count++;
+      }
+    }
+    if (count > n / 2) {
+      return arr[i];
+    }
+  }
+  return null;
+}
+
+function findMajorityElementUsingHashMap(arr) {
+  let frequencyMap = new Map();
+  let n = arr.length;
+  for (let num of arr) {
+    frequencyMap.set(num, (frequencyMap.get(num) || 0) + 1);
+    if (frequencyMap.get(num) > n / 2) {
+      return num;
+    }
+  }
+  return null;
+}
+
+function findMajorityElementBoyerMoore(arr) {
+  let count = 0;
+  let candidate = null;
+  for (let num of arr) {
+    if (count == 0) {
+      candidate = num;
+    }
+    count += candidate == num ? 1 : -1;
+   }
+   
+  count = 0;
+  let n = arr.length;
+  for (let num of arr) {
+    if (num == candidate) {
+      count++;
+    }
+  if (count > n / 2) {
+    return num;
+  }
+  }
+  return null;
+}
+
+let arr = [3, 3, 4, 2, 4, 4, 2, 4, 4];
+console.log(findMajorityElementBoyerMoore(arr)); // Output: 4
